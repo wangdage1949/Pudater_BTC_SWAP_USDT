9763571aa1f1a8e12b1a4304811b565a766e7697
王大哥 <tcwdy04@gmail.com>
2026-03-11 17:05:31 +0800

    Update qidong.bat

--- qidong.bat
+++ qidong.bat
@@ -1,1 +1,28 @@
-python main.py
+@echo off
+title Pudater_gendan_bot by_Wangdage
+
+echo ============================================
+echo     Pudater_gendan_bot by_Wangdage
+echo ============================================
+echo.
+
+:: Switch to current folder
+cd /d %~dp0
+
+:: Check Python
+python --version >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo [ERROR] Python is not installed!
+    pause
+    exit /b
+)
+
+echo Starting  Pudater_gendan_bot by_Wangdage ...
+echo.
+
+:: Start waigua.py
+python waigua.py
+
+echo.
+echo Worker exited. Press any key to close.
+pause
